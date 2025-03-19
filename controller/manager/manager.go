cce861eb352d19013855b87b76cf0daea5a86d92
marcosadsj <marcosadsj@gmail.com>
2025-03-18 21:58:37 -0300

    feat: add endpoint update and delete

--- controller/manager/manager.go
+++ controller/manager/manager.go
@@ -1,7 +1,9 @@
 package manager
 
 import (
+	"fmt"
 	"net/http"
+	"strconv"
 	"sword-health-assessment/entities"
 	managerService "sword-health-assessment/services/manager"
 
@@ -22,14 +24,14 @@ func (mc ManagerController) Controller(httpServer *gin.Engine, service *managerS
 
 	routeGroup := httpServer.Group("/manager")
 
-	routeGroup.POST("/create", mc.Create)
-	routeGroup.GET("/findById/:id", mc.Read)
-	routeGroup.PUT("/update/:id", mc.Update)
-	routeGroup.DELETE("/delete/:id", mc.Delete)
+	routeGroup.POST("/create", mc.create)
+	routeGroup.GET("/findById/:id", mc.read)
+	routeGroup.PUT("/update", mc.update)
+	routeGroup.DELETE("/delete/:id", mc.delete)
 
 }
 
-func (mc ManagerController) Create(ctx *gin.Context) {
+func (mc ManagerController) create(ctx *gin.Context) {
 
 	manager := entities.Manager{}
 
@@ -46,14 +48,78 @@ func (mc ManagerController) Create(ctx *gin.Context) {
 
 }
 
-func (mc ManagerController) Read(ctx *gin.Context) {
+func (mc ManagerController) read(ctx *gin.Context) {
+
+	idString := ctx.Param("id")
+
+	if idString == "" {
+
+		ctx.String(http.StatusBadRequest, "Empty request param")
+
+		return
+	}
+
+	id, err := strconv.ParseInt(idString, 10, 32)
+
+	if err != nil {
+
+		ctx.String(http.StatusBadRequest, fmt.Sprintf("Wrong request param: %v", id))
+
+		return
+	}
+
+	managers, err := mc.service.Read([]int{int(id)})
+
+	if err != nil {
+		ctx.String(http.StatusInternalServerError, fmt.Sprintf("Error to find manager(s): %v", err))
+
+		return
+	}
+
+	ctx.JSON(http.StatusOK, managers)
+}
+
+func (mc ManagerController) update(ctx *gin.Context) {
+
+	manager := entities.Manager{}
+
+	if err := ctx.ShouldBindBodyWithJSON(&manager); err == nil {
+
+		mc.service.Update(&manager)
+
+		ctx.String(http.StatusOK, "Manager updated")
+
+		return
+	}
+
+	ctx.String(http.StatusInternalServerError, "Manager not updated")
 
 }
 
-func (mc ManagerController) Update(ctx *gin.Context) {
+func (mc ManagerController) delete(ctx *gin.Context) {
 
-}
+	idString := ctx.Param("id")
 
-func (mc ManagerController) Delete(ctx *gin.Context) {
+	if idString != "" {
+
+		id, err := strconv.ParseInt(idString, 10, 32)
+
+		if err != nil {
+
+			ctx.String(http.StatusBadRequest, fmt.Sprintf("Wrong request param: %v", id))
+
+			return
+		}
+
+		err = mc.service.Delete([]int{int(id)})
+
+		if err != nil {
+			ctx.String(http.StatusInternalServerError, fmt.Sprintf("Error on delete Manager: %v", err))
+			return
+		}
+
+		ctx.String(http.StatusOK, fmt.Sprintf("Manager deleted id: %d", id))
+
+	}
 
 }
