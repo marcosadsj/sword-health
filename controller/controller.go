5a12a85569510144fb9995063a38a117bc4ff999
marcosadsj <marcosadsj@gmail.com>
2025-03-18 23:05:24 -0300

    feat: initialize all controllers

--- controller/controller.go
+++ controller/controller.go
@@ -2,8 +2,17 @@ package api
 
 import (
 	managerController "sword-health-assessment/controller/manager"
+	taskController "sword-health-assessment/controller/task"
+	technicianController "sword-health-assessment/controller/technician"
+
 	managerRepository "sword-health-assessment/repository/manager"
 	managerService "sword-health-assessment/services/manager"
+
+	technicianRepository "sword-health-assessment/repository/technician"
+	technicianService "sword-health-assessment/services/technician"
+
+	taskRepository "sword-health-assessment/repository/task"
+	taskService "sword-health-assessment/services/task"
 
 	"github.com/gin-gonic/gin"
 	"gorm.io/gorm"
@@ -11,15 +20,32 @@ import (
 
 func Init(httpServer *gin.Engine, db *gorm.DB) {
 
-	mRepository := &managerRepository.ManagerRepository{
-		DB: db,
-	}
+	managerRepository := &managerRepository.ManagerRepository{DB: db}
 
-	mService := &managerService.ManagerService{}
+	techRepository := &technicianRepository.TechnicianRepository{DB: db}
 
-	mService.New(mRepository)
+	tRepository := &taskRepository.TaskRepository{DB: db}
+
+	managerService := &managerService.ManagerService{}
+
+	managerService.New(managerRepository)
+
+	techService := &technicianService.TechnicianService{}
+
+	techService.New(techRepository)
+
+	tService := &taskService.TaskService{}
+
+	tService.New(tRepository)
 
 	mc := &managerController.ManagerController{}
 
-	mc.Controller(httpServer, mService)
+	techC := &technicianController.TechnicianController{}
+
+	taskC := &taskController.TaskController{}
+
+	mc.Controller(httpServer, managerService)
+	techC.Controller(httpServer, techService)
+	taskC.Controller(httpServer, tService)
+
 }
