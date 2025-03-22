e187843f6f8c6a3db90a4bd25829897c12f2567b
marcosadsj <marcosadsj@gmail.com>
2025-03-21 23:38:07 -0300

    feat: add factory to database types

---
+++ internal/database/databases.go
@@ -0,0 +1,29 @@
+package database
+
+import (
+	"sword-health-assessment/internal/database/mysql"
+	"sword-health-assessment/internal/database/sqlite"
+
+	"gorm.io/gorm"
+)
+
+type IDatabase interface {
+	New(enviroment string)
+	Connect()
+	Close()
+	Repository()
+	Migrate()
+	GetDB() *gorm.DB
+	SetEnviroment(string)
+}
+
+func Create(databaseType string) IDatabase {
+	switch databaseType {
+	case "SQLITE":
+		return &sqlite.SQLite{}
+	case "MYSQL":
+		return &mysql.MySQL{}
+	}
+
+	return &sqlite.SQLite{}
+}
